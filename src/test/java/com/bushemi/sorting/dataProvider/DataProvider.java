26bb9d1bad3281e5337028225d6f5686a101af53
Igor <bushemi2001@gmail.com>
2019-02-02 11:26:41 +0200

    was added more numbers to the sortable list.

--- src/test/java/com/bushemi/sorting/dataProvider/DataProvider.java
+++ src/test/java/com/bushemi/sorting/dataProvider/DataProvider.java
@@ -8,6 +8,6 @@ public final class DataProvider {
     }
 
     public static List<Integer> getArrayListOfIntegers() {
-        return Arrays.asList(5, 4, 8, 9, 1, 6, 2);
+        return Arrays.asList(5, 4, 8, 9, 1, 6, 2,10,20,11,23,99,7,16,19,15);
     }
 }
