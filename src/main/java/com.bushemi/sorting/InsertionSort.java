e7ee75e0e40777f1abb16cd78b1a2029b3780750
Igor <bushemi2001@gmail.com>
2019-02-02 10:52:29 +0200

    was added a new insertion sort class with test for it

---
+++ src/main/java/com.bushemi/sorting/InsertionSort.java
@@ -0,0 +1,11 @@
+package com.bushemi.sorting;
+
+import java.util.List;
+
+public class InsertionSort implements SorterService{
+
+    @Override
+    public List<Integer> sort(List<Integer> numbers) {
+        return numbers;
+    }
+}
---
+++ src/test/java/com/bushemi/sorting/InsertionSortTest.java
@@ -0,0 +1,30 @@
+package com.bushemi.sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.List;
+
+import static com.bushemi.sorting.dataProvider.DataProvider.getArrayListOfIntegers;
+
+@RunWith(JUnit4.class)
+public class InsertionSortTest {
+    @Test
+    public void sort() {
+        SorterService sorter = new InsertionSort();
+        List<Integer> numbers = getArrayListOfIntegers();
+        System.out.println("numbers = " + numbers);
+        List<Integer> sortedList =
+                sorter.sort(numbers);
+        System.out.println("sortedList = " + sortedList);
+        for (int i = 0; i < sortedList.size() - 1; i++) {
+            int x = sortedList.get(i);
+            int y = sortedList.get(i + 1);
+            Assert.assertTrue("list is not sorted", x < y);
+        }
+    }
+
+
+}
